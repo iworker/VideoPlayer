d612e8ea4b4ae275cdc804adf25bbde24e755848
Ivan Pomortsev <ivan.pomortsev@gmail.com>
2023-12-06 23:07:35 +0400

    update dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-      .package(url: "https://github.com/iworker/GSPlayer.git", from: "0.2.27-selfplus"),
+      .package(url: "https://github.com/iworker/GSPlayer.git", from: "0.2.28-selfplus"),
     ],
     targets: [
         .target(
